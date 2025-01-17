7526d5a85a68ec4a8e49b59ceac2bb60b628fefa
Frédéric Penel <frederic.penel@gmx.fr>
2025-01-17 11:29:58 +0100

    update uri

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
-import { ImperativeMultiplicationComponent } from './imperative-multiplication/imperative-multiplication.component';
-import { SignalMultiplicationComponent } from './signal-multiplication/signal-multiplication.component';
-import { RxjsMultiplicationComponent } from './rxjs-multiplication/rxjs-multiplication.component';
+import { ImperativeMultiplicationComponent } from './multiplication/imperative-multiplication/imperative-multiplication.component';
+import { SignalMultiplicationComponent } from './multiplication/signal-multiplication/signal-multiplication.component';
+import { RxjsMultiplicationComponent } from './multiplication/rxjs-multiplication/rxjs-multiplication.component';
 
 export const routes: Routes = [
-    { path: 'imperative-multiplication', component: ImperativeMultiplicationComponent },
-    { path: 'rxjs-multiplication', component: RxjsMultiplicationComponent },
-    { path: 'signal-multiplication', component: SignalMultiplicationComponent },
-    { path: '**', redirectTo: '/imperative-multiplication', pathMatch: 'full' }
-  
+    { path: 'multiplication', children: [
+        {path: 'imperative', component: ImperativeMultiplicationComponent},
+        {path: "rxjs", component: RxjsMultiplicationComponent},
+        {path: "signal", component: SignalMultiplicationComponent},
+        {path: "**", redirectTo: "imperative"}
+        ]
+    },
+    { path: '**', redirectTo: '/multiplication/imperative', pathMatch: 'full' }
 ];
